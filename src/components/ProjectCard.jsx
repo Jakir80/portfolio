df8cb53b4684fe25dede20db2c4e8d144ed419ab
Jakir80 <jh7593617@gmail.com>
2023-06-26 16:16:51 +0600

    link

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -8,8 +8,10 @@ function ProjectCard({ item }) {
         <div className="p absolute top-[-100%] left-0 h-full w-full text-center bg-[#b004b0b7] p-4 pt-20">
           <h1 className="font-bold text-3xl">{item.title}</h1>
           <p className="font-bold">{item.description}</p>
-          <button className='border-2 border-[#b0aaaa]  text-white bg-[linear-gradient(90deg,#b004b0,#38097a)] p-2 rounded-2xl'>Github </button>
-          <button className=' m-2 border-2 border-[#b0aaaa]  text-white bg-[linear-gradient(90deg,#b004b0,#38097a)] p-2 rounded-2xl'>Live Demo</button>
+        <a href={item.github}>
+        <button className='border-2 border-[#b0aaaa]  text-white bg-[linear-gradient(90deg,#b004b0,#38097a)] p-2 rounded-2xl'>Github </button>
+        </a>
+        <a href={item.demo}>  <button className=' m-2 border-2 border-[#b0aaaa]  text-white bg-[linear-gradient(90deg,#b004b0,#38097a)] p-2 rounded-2xl'>Live Demo</button></a>
         </div>
       </div>
     </>
